aee4f68b7e4e502dc97fc815016d10134ce5c5c4
Luke Henwood <luke8h@gmail.com>
2017-04-22 15:14:41 -0500

    using filter to find values less than 6

---
+++ main.js
@@ -0,0 +1,18 @@
+
+var oldArray = [1,2,3,4,5,6,7,8,9,10];
+
+// Only change code below this line.
+
+var newArray = oldArray.filter(function(val){
+ return val < 6;
+});
+ console.log(newArray);
+
+
+
+
+// array = array.filter(function(val) {
+//   return val !== 5;
+// });
+// Use filter to create a new array with all the values
+// from oldArray which are less than 6. The oldArray should not change.
